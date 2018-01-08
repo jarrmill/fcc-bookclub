e8fc1ba6e86494147ab49dd9e887e82293c1f372
Jarrod <jarrod.miller@ttu.edu>
2018-01-07 19:41:31 -0700

    .

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'jmillie.auth0.com',
   clientId: 'A0WNgpsJ86OpgzFzFiwoSX0fls7KZcH3',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://jsfccbookclub.herokuapp.com/callback',
   apiUrl: 'https://jmillie.auth0.com/api/v2/'
 }
